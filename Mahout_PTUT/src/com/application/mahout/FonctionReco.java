a2519cb7e7c7877f6eb485032a153e2a0225b6d7
BastienKovac <bastien.kovac@gmail.com>
2015-11-23 21:13:16 +0100

    Modification de l'interface FonctionReco

--- Mahout_PTUT/src/com/application/mahout/FonctionReco.java
+++ Mahout_PTUT/src/com/application/mahout/FonctionReco.java
@@ -2,12 +2,10 @@ package com.application.mahout;
 
 import java.util.List;
 
-import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
-import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 
 public interface FonctionReco {
 	
-	public List<RecommendedItem> getRecommendedItems(DataModel model, ItemSimilarity sim) ;
+	public List<RecommendedItem> getRecommendedItems() ;
 
 }
